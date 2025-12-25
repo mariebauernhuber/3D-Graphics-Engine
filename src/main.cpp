81e4075de704332b7cb60da203434b25ce4aa4f4
Marie Bauernhuber <marie.bauernhuber@proton.me>
2025-12-25 01:59:13 +0100

    Basic culling, still needs fixing

--- src/main.cpp
+++ src/main.cpp
@@ -180,51 +180,76 @@ SDL_AppResult SDL_AppIterate(void *appstate){
 
     for(auto tri : meshCube.tris) {
         triangle triProjected, triTranslated, triRotatedZ, triRotatedX, triRotatedZX;
-
+        
+        // Rotate in Y-Axis
         MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
         MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
         MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
 
+        // Rotate in X-Axis
         MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
         MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
         MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
         
+        //Offset onto the screen
         triTranslated = triRotatedZX;
         triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
         triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
         triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+        
+        vec3d normal, line1, line2;
+        line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
+        line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
+        line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
 
-        MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
-        MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
-        MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+        line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
+        line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
+        line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
 
-        triProjected.p[0].x = (triProjected.p[0].x + 1.0f) * 0.5f * windowWidth;
-        triProjected.p[0].y = (1.0f - (triProjected.p[0].y + 1.0f) * 0.5f) * windowHeight;
+        normal.x = line1.y * line2.z - line1.z * line2.y;
+        normal.y = line1.z * line2.x - line1.x * line2.z;
+        normal.z = line1.x * line2.y - line1.y * line2.x;
 
-        triProjected.p[1].x = (triProjected.p[1].x + 1.0f) * 0.5f * windowWidth;
-        triProjected.p[1].y = (1.0f - (triProjected.p[1].y + 1.0f) * 0.5f) * windowHeight;
+        float l = sqrtf(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+        normal.x /= l;
+        normal.y /= l;
+        normal.z /= l;
 
-        triProjected.p[2].x = (triProjected.p[2].x + 1.0f) * 0.5f * windowWidth;
-        triProjected.p[2].y = (1.0f - (triProjected.p[2].y + 1.0f) * 0.5f) * windowHeight;
+        if(normal.z < 0) {
+            // Project Tringles (3D -> 2D)
+            MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+            MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+            MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+
+            // Scale into view
+            triProjected.p[0].x = (triProjected.p[0].x + 1.0f) * 0.5f * windowWidth;
+            triProjected.p[0].y = (1.0f - (triProjected.p[0].y + 1.0f) * 0.5f) * windowHeight;
+
+            triProjected.p[1].x = (triProjected.p[1].x + 1.0f) * 0.5f * windowWidth;
+            triProjected.p[1].y = (1.0f - (triProjected.p[1].y + 1.0f) * 0.5f) * windowHeight;
+
+            triProjected.p[2].x = (triProjected.p[2].x + 1.0f) * 0.5f * windowWidth;
+            triProjected.p[2].y = (1.0f - (triProjected.p[2].y + 1.0f) * 0.5f) * windowHeight;
 
 
-        SDL_RenderLine(
-        renderer,
-        triProjected.p[0].x, triProjected.p[0].y,
-        triProjected.p[1].x, triProjected.p[1].y
-        );
+            SDL_RenderLine(
+            renderer,
+            triProjected.p[0].x, triProjected.p[0].y,
+            triProjected.p[1].x, triProjected.p[1].y
+            );
 
-        SDL_RenderLine(
-        renderer,
-        triProjected.p[1].x, triProjected.p[1].y,
-        triProjected.p[2].x, triProjected.p[2].y
-        );
+            SDL_RenderLine(
+            renderer,
+            triProjected.p[1].x, triProjected.p[1].y,
+            triProjected.p[2].x, triProjected.p[2].y
+            );
 
-        SDL_RenderLine(
-        renderer,
-        triProjected.p[2].x, triProjected.p[2].y,
-        triProjected.p[0].x, triProjected.p[0].y
-        );
+            SDL_RenderLine(
+            renderer,
+            triProjected.p[2].x, triProjected.p[2].y,
+            triProjected.p[0].x, triProjected.p[0].y
+            );
+        };
 
     };
 
